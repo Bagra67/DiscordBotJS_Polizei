90bee40bacc6fab0046ea2d299062ebca96a0c38
ladwein <ladwein@etu.unistra.fr>
2021-06-23 14:44:14 +0200

    initial code

---
+++ src/index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const { Client, WebhookClient } = require('discord.js');
+
+const client = new Client({
+  partials: ['MESSAGE', 'REACTION']
+});
+
+const webhookClient = new WebhookClient(
+  process.env.WEBHOOK_ID,
+  process.env.WEBHOOK_TOKEN,
+);
+
+const PREFIX = "$";
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('message', async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    if (CMD_NAME === 'ping') {
+     
+    } else if (CMD_NAME === 'pong') {
+      
+    } 
+  }
+});
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
