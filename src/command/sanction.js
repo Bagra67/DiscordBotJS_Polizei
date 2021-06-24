97356fc1c3bd59643284a36c03f5970b40be3421
ladwein <ladwein@etu.unistra.fr>
2021-06-24 11:22:13 +0200

    structure models add plugin expres etc

--- src/command/sanction.js
+++ src/command/sanction.js
@@ -1,35 +1,36 @@
 const { Client } = require('discord.js');
 const api = require('../api')
 var answerme_list_user_activate = []
+
 module.exports = {
     answerme: (client,message, args) => {
         const user = api.getUserFromMention(client,args[0])
 
         if(answerme_list_user_activate.find(item => item.id = user.id) != undefined){
-            answerme_list_user_activate.map((item, i) =>{
+            answerme_list_user_activate.map((item, i) => {
                 answerme_list_user_activate[i].allow = item.id === user.id ? args[1] === 'true' : item.allow
             })
         }else{
             answerme_list_user_activate.push({
-                id:user.id,
+                id: user.id,
                 allow: args[1] === 'true'
             })
         }
        
-            client.on('message',(message) => {
-                const emoji_answerme = message.guild.emojis.cache.get('784834794462248980');
-                const answerme_item = answerme_list_user_activate.find(item => item.id === message.author.id)
+        client.on('message',(message) => {
+            const emoji_answerme = message.guild.emojis.cache.get('784834794462248980');
+            const answerme_item = answerme_list_user_activate.find(item => item.id === message.author.id)
 
-                if(answerme_item !== undefined){
-                    if(answerme_item.allow){
-                        message.react(emoji_answerme)
-                        .then((response) => console.log("Emoji added"))
-                        .catch((err) => console.log("Error on answerme reaction"))
-                    }
-                    
+            if(answerme_item !== undefined){
+                if(answerme_item.allow){
+                    message.react(emoji_answerme)
+                    .then((response) => console.log("Emoji added"))
+                    .catch((err) => console.log("Error on answerme reaction"))
                 }
+                
+            }
 
-            })
+        })
             
     },
     
